1417e0871153ce8905a72f95a455720e6658f40f
Conor Higgins <conor.higgins@teamwork.com>
2020-07-13 21:27:49 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,13 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'Now deploying'
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:conorhiggins/conorhiggins.github.io.git master
 
 cd -
