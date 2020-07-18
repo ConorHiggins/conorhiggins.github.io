5c439a4513ef6483a60a6b7f4c398cbff55ba972
Conor Higgins <conor.higgins@teamwork.com>
2020-07-18 15:47:50 +0100

    update webpack config Update gitignore

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,27 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin');
+
+let plugins = [];
+if (process.env.NODE_ENV === "production") {
+  const compressionTest = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
+  plugins = [
+    new CompressionPlugin({
+      algorithm: 'gzip',
+      test: compressionTest,
+      threshold: 10240,
+      minRatio: 0.7
+    }),
+    new BrotliPlugin({
+      test: compressionTest,
+      threshold: 10240,
+      minRatio: 0.7
+    })
+  ];
+}
+
 module.exports = {
   publicPath: './',
+  configureWebpack: {
+    plugins
+  },
 }
